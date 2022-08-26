d76bcde2a6d138855ef75fd980f4c1b2552a946e
ShengUei <shengueiwengtw@gmail.com>
2022-08-27 01:03:21 +0800

    if scheduler error, send mail to admin

--- schedule.py
+++ schedule.py
@@ -1,9 +1,21 @@
 from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime, timezone
+
 from schedule.jobs.jobs import get_and_save_exchange_rate
+from sendEmail.send_email import send_email
 
 scheduler = BackgroundScheduler()
 
-#每週一 ~ 五 18:00 ，由網路抓匯率與存匯率至DB
-scheduler.add_job(get_and_save_exchange_rate, 'cron', day_of_week = '1-5', hour = 18, minute = 0, timezone = 'utc+8')
+try:
+    #每週一 ~ 五 18:00 ，由網路抓匯率與存匯率至DB
+    scheduler.add_job(get_and_save_exchange_rate, 'cron', day_of_week = '1-5', hour = 18, minute = 0, timezone = 'utc+8')
+    scheduler.start()
 
-scheduler.start()
+except Exception as e:
+    scheduler.shutdown()
+
+    print("Exception %s at %s", e, datetime.now(timezone.utc))
+
+    title = "Run scheduler Failure"
+    content = "Run scheduler Failure at {}".format(datetime.now(timezone.utcoffset(+8)))
+    send_email(title, content)
