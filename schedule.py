1a199f502ba41b3b6abaca116eb69e5c4e3a5077
ShengUei <shengueiwengtw@gmail.com>
2022-08-28 14:54:24 +0800

    add show_running

--- schedule.py
+++ schedule.py
@@ -2,6 +2,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime, timezone
 
 from schedule.exchange_rate_job import get_and_save_exchange_rate
+from schedule.running import show_running
 from sendEmail.send_email import send_email
 
 scheduler = BackgroundScheduler()
@@ -9,6 +10,7 @@ scheduler = BackgroundScheduler()
 try:
     #每週一 ~ 五 18:00 ，由網路抓匯率與存匯率至DB
     scheduler.add_job(get_and_save_exchange_rate, 'cron', day_of_week = '1-5', hour = 18, minute = 0, timezone = 'Asia/Taipei')
+    scheduler.add_job(show_running,'interval', minutes = 1)
     scheduler.start()
 
 except Exception as e:
