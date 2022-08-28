9421c28d6cbfd9925fbe92d4e60c6c4c953e2ab2
ShengUei <shengueiwengtw@gmail.com>
2022-08-28 16:21:18 +0800

    fix datetime

--- schedule.py
+++ schedule.py
@@ -1,29 +1,26 @@
 from apscheduler.schedulers.background import BackgroundScheduler
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 from schedule.exchange_rate_job import get_and_save_exchange_rate
-from schedule.running import show_running
 from sendEmail.send_email import send_email
 
 scheduler = BackgroundScheduler()
 
-print("Run schedule.py at %s", datetime.now(timezone.tzname('Asia/Taipei')))
+print("Run schedule.py at %s", datetime.now(timezone.utc))
 
 try:
-    print("Add Jobs to Scheduler at %s", datetime.now(timezone.tzname('Asia/Taipei')))
+    print("Add Jobs to Scheduler at %s", datetime.now(timezone.utc))
 
     #每週一 ~ 五 18:00 ，由網路抓匯率與存匯率至DB
     scheduler.add_job(get_and_save_exchange_rate, 'cron', day_of_week = '1-5', hour = 18, minute = 0, timezone = 'Asia/Taipei')
-    scheduler.add_job(show_running,'interval', minutes = 60)
-
-    print("Start Scheduler at %s", datetime.now(timezone.tzname('Asia/Taipei')))
     scheduler.start()
 
 except Exception as e:
     scheduler.shutdown()
 
-    print("Exception %s at %s", e, datetime.now(timezone.utc))
+    now = datetime.now(timezone.utc)
+    local_datetime = now + timedelta(hours = 8)
 
     title = "Run scheduler Failure"
-    content = "Run scheduler Failure at {}".format(datetime.now(timezone.tzname('Asia/Taipei')))
+    content = "Run scheduler Failure at {}".format(local_datetime.strftime('%Y/%m/%d %H:%M:%S'))
     send_email(title, content)
--- schedule/exchange_rate_job.py
+++ schedule/exchange_rate_job.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from sendEmail.send_email import send_email
 from webCrawler.foreign_exchange_rate import get_daily_rate
 from dataAccess.postgresql.data_access import insert_all_exchange_rate
@@ -7,13 +7,19 @@ def get_and_save_exchange_rate():
     dailyRateList = get_daily_rate()
 
     if not dailyRateList:
+        now = datetime.now(timezone.utc)
+        local_datetime = now + timedelta(hours = 8)
+
         title = "Get Daily Exchange Rate Failure From web"
-        content = "Get Daily Exchange Rate Failure From web at {}".format(datetime.now(timezone.utcoffset(+8)))
+        content = "Get Daily Exchange Rate Failure From web at {}".format(local_datetime.strftime('%Y/%m/%d %H:%M:%S'))
         send_email(title, content)
     
     else:
         insert_all_exchange_rate(dailyRateList)
 
+        now = datetime.now(timezone.utc)
+        local_datetime = now + timedelta(hours = 8)
+
         title = "Get Daily Exchange Rate Success"
-        content = "Get Daily Exchange Rate Success at {}".format(datetime.now(timezone.utcoffset(+8)))
+        content = "Get Daily Exchange Rate Success at {}".format(local_datetime.strftime('%Y/%m/%d %H:%M:%S'))
         send_email(title, content)
