7926f4cbebbdd05e075ec41f817943e02b983f0a
ShengUei <shengueiwengtw@gmail.com>
2022-08-28 15:29:39 +0800

    add print

--- schedule.py
+++ schedule.py
@@ -7,10 +7,16 @@ from sendEmail.send_email import send_email
 
 scheduler = BackgroundScheduler()
 
+print("Run schedule.py at %s", datetime.now(timezone.utcoffset(+8)))
+
 try:
+    print("Add Jobs to Scheduler at %s", datetime.now(timezone.utcoffset(+8)))
+
     #每週一 ~ 五 18:00 ，由網路抓匯率與存匯率至DB
     scheduler.add_job(get_and_save_exchange_rate, 'cron', day_of_week = '1-5', hour = 18, minute = 0, timezone = 'Asia/Taipei')
     scheduler.add_job(show_running,'interval', minutes = 60)
+
+    print("Start Scheduler at %s", datetime.now(timezone.utcoffset(+8)))
     scheduler.start()
 
 except Exception as e:
