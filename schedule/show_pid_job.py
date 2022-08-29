9509a1afac64f18bf84024909edd1b57917b73e6
ShengUei <shengueiwengtw@gmail.com>
2022-08-29 20:44:29 +0800

    at time

--- schedule/show_pid_job.py
+++ schedule/show_pid_job.py
@@ -1,12 +1,16 @@
 import threading
+from datetime import datetime, timezone, timedelta
 
 #設定目錄
 dir_path = '/home/'
 
 def show_pid():
     pid = threading.get_native_id()
-    print('PID: %s' %pid)
     filename = 'pid_output.txt'
-    f = open(dir_path + filename, 'w')
-    f.write('PID: %s' % pid)
+
+    now = datetime.now(timezone.utc)
+    local_datetime = now + timedelta(hours = 8)
+
+    f = open(dir_path + filename, 'a')
+    f.write('PID: %s at %s \n' % (pid, local_datetime))
     f.close()
