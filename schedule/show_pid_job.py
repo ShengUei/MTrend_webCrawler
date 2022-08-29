5bf3179824792b20760f897d6562deb7a8a9a888
ShengUei <shengueiwengtw@gmail.com>
2022-08-29 20:26:56 +0800

    print

--- schedule/show_pid_job.py
+++ schedule/show_pid_job.py
@@ -4,7 +4,8 @@ import threading
 dir_path = '/home/'
 
 def show_pid():
-    pid = threading.get_native_id
+    pid = threading.get_native_id()
+    print('PID: %s' %pid)
     filename = 'pid_output.txt'
     f = open(dir_path + filename, 'w')
     f.write('PID: %s' % pid)
