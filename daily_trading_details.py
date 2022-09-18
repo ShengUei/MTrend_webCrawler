046cab79c8f4529c3d1c88de1d8f47eb6222da58
ShengUei <shengueiwengtw@gmail.com>
2022-09-18 14:51:05 +0800

    some test

--- daily_trading_details.py
+++ daily_trading_details.py
@@ -10,11 +10,25 @@ date_str = local_datetime.strftime('%Y%m%d')
 
 HOST = 'www.twse.com.tw'
 url = 'https://www.twse.com.tw/en/fund/T86?response=json&date={0}&selectType=ALL'.format(date_str)
-stat_code = {0: 'OK', 1: 'No Data!'}
 
-res_dict = get_request(url, HOST)
+def test_method(url, HOST):
+    dict = {'data': None, 'error_msg': None}
+    stat_code = {0: 'OK', 1: 'No Data!'}
+    res_dict = get_request(url, HOST)
 
-if(res_dict['stat'] == stat_code[0]):
-    print(res_dict['data'])
-else:
-    print('No Data!')
+    if(local_weekday > 5):
+        dict['error_msg'] = 'Today is holiday.'
+        return dict
+
+    if(res_dict['stat'] != stat_code[0]):
+        dict['error_msg'] = 'No data.'
+        return dict
+
+    dict['data'] = res_dict['data']
+    
+    return dict
+
+result = test_method(url, HOST)
+
+if(result['data'] == None):
+    print(result['error_msg'])
