519465ddec81ad81584504b95dd694cb551b7bd7
ShengUei <shengueiwengtw@gmail.com>
2022-09-18 15:35:34 +0800

    temp

--- daily_trading_details.py
+++ daily_trading_details.py
@@ -1,34 +1,45 @@
 from datetime import datetime, timedelta
-from threading import local
 
 from util.request import get_request
+from logger.logger import get_logger
 
-utc_now = datetime.utcnow()
-local_datetime = utc_now + timedelta(hours = 8)
-local_weekday = local_datetime.isoweekday()
-date_str = local_datetime.strftime('%Y%m%d')
+def get_daily_trading_details():
+    utc_now = datetime.utcnow()
+    local_datetime = utc_now + timedelta(hours = 8)
+    local_weekday = local_datetime.isoweekday()
+    date_str = local_datetime.strftime('%Y%m%d')
 
-HOST = 'www.twse.com.tw'
-url = 'https://www.twse.com.tw/en/fund/T86?response=json&date={0}&selectType=ALL'.format(date_str)
-
-def test_method(url, HOST):
-    dict = {'data': None, 'error_msg': None}
+    HOST = 'www.twse.com.tw'
+    url = 'https://www.twse.com.tw/en/fund/T86?response=json&date={0}&selectType=ALL'.format(date_str)
     stat_code = {0: 'OK', 1: 'No Data!'}
-    res_dict = get_request(url, HOST)
 
-    if(local_weekday > 5):
-        dict['error_msg'] = 'Today is holiday.'
-        return dict
+    logger = get_logger()
 
-    if(res_dict['stat'] != stat_code[0]):
-        dict['error_msg'] = 'No data.'
-        return dict
+    try:
+        if(local_weekday > 5):
+            print('Today is holiday.')
+            logger.info('Today is holiday.')
+            return None
 
-    dict['data'] = res_dict['data']
-    
-    return dict
+        res_dict = get_request(url, HOST)
 
-result = test_method(url, HOST)
+        if(res_dict == None):
+            print('Get Data Failures From web.')
+            logger.error('Get Data Failures From web.')
+            return None
+        
+        if(res_dict['stat'] != stat_code[0]):
+            print('No data.')
+            logger.error('No data.')
+            return None
 
-if(result['data'] == None):
-    print(result['error_msg'])
+    except Exception as e:
+        print("Excetion $s", e)
+        logger.error("Exception : %s" % e)
+        return None
+
+    else:
+        print("Get Daily Trading Details Success From web")
+        logger.info("Get Daily Trading Details Success From web")
+        return res_dict['data']
+
--- util/request.py
+++ util/request.py
@@ -1,13 +1,22 @@
 import random
 import requests
 import json
+import time
+
+from logger.logger import get_logger
 
 CHROME = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'
 EDGE = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/104.0.1293.70'
 web_browser_list = [CHROME, EDGE]
 
 def get_request(target_url, host):
-    res = requests.get(url = target_url,
+    try:
+        #先 sleep 幾秒，再開始 run
+        time.sleep(random.uniform(1.0, 60.0))
+
+        logger = get_logger()
+
+        res = requests.get(url = target_url,
                     headers = {"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9,application/json,text/javascript, */*; q=0.01", 
                                 "Accept-Encoding": "gzip, deflate, br", 
                                 "Accept-Language": "zh-TW,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6", 
@@ -19,4 +28,18 @@ def get_request(target_url, host):
                                 "user-agent" : random.choice(web_browser_list),
                                 "Referer": "https://www.google.com/"})
     
-    return json.loads(res.text)
+    except Exception as e:
+        print("Excetion $s", e)
+        logger.error("BaseException : %s" % e)
+        return None
+
+    else:
+        print("Get Data Success From web")
+        logger.info("Get Data Success From web")
+        return json.loads(res.text)
+    
+    finally:
+        res.close()
+
+    
+    
