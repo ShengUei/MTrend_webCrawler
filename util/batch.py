f735f3f8e90bca2485bae6f2ff5b64e05d60b84e
ShengUei <shengueiwengtw@gmail.com>
2022-09-20 22:47:41 +0800

    need to test

--- util/batch.py
+++ util/batch.py
@@ -1,13 +1,9 @@
 import math
 
-def test(number):
-    print(number)
-
 def insert_batch(func, data_list):
     max = len(data_list)
     gap = 10
-    print('(max % gap)')
-    print((max % gap))
+
     if (max % gap) == 0.0:
         step = math.floor(max / gap)
     else:
@@ -16,8 +12,6 @@ def insert_batch(func, data_list):
     start = 0
 
     for n in range(step):
-        print('step')
-        print(step)
         if n == (step - 1):
             end = max
         else:
@@ -27,8 +21,4 @@ def insert_batch(func, data_list):
             func(data_list[i])
 
         start = end
-        print('==========')
 
-data_list = range(0, 99)
-
-insert_batch(test, data_list)
