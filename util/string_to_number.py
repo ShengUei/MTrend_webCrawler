663b2c20cc8f7727f001821c7e76f8dea526d377
ShengUei <shengueiwengtw@gmail.com>
2022-09-20 22:01:27 +0800

    debug strToInt

--- util/string_to_number.py
+++ util/string_to_number.py
@@ -1,6 +1,10 @@
 
 def str_to_int(str_int):
-    return int(str_int.replace(',', ''))
+    if type(str_int) == str:
+        return int(str_int.replace(',', ''))
+    return str_int
 
 def str_to_float(str_float):
-    return float(str_float.replace(',', ''))
+    if type(str_float) == str:
+        return float(str_float.replace(',', ''))
+    return str_float
